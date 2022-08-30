bc6cf65db596ad0c086536596bc8c2462c61e0a6
boni <kgb8594@gmail.com>
2022-08-30 15:09:18 +0900

    Pagination UI에 적용해보기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,6 +1,8 @@
 import 'package:actual/common/const/data.dart';
+import 'package:actual/common/dio/dio.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:actual/restaurant/repository/restaurant_repository.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -11,18 +13,11 @@ class RestaurantScreen extends StatelessWidget {
   Future<List> paginateRestaurant() async {
     final dio = Dio();
 
+    dio.interceptors.add(CustomInterceptor(storage));
+
     final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
-    final repo = await dio.get(
-      'http://$ip/restaurant',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
-      ),
-    );
-
-    return repo.data['data'];
+    return RestaurantRepository(dio).paginate();
   }
 
   @override
