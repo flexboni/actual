90fcb95087c7e2d6cbe964ee06ecd9f8afb0e846
boni <kgb8594@gmail.com>
2022-08-31 14:56:52 +0900

    CursorPagination 상태 작업하기

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -1,9 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 
 part 'cursor_pagination_model.g.dart';
 
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
+
 @JsonSerializable(genericArgumentFactories: true)
-class CursorPagination<T> {
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -29,4 +42,22 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+// 새로고침 할 때
+// 이미 CursorPagination 이 존재하는 상태에서 다시 불러오니까
+// CursorPagination 를 상속하는 것
+class CursorPaginationRefetching<T> extends CursorPagination<T> {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+// 리스트의 맨 아래로 내려서 추가 데이터를 요청하는 중
+class CursorPaginationFetchingMore<T> extends CursorPagination<T> {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
