3df79ac71e83b5291184bccac59aa12f6a643398
boni <kgb8594@gmail.com>
2022-09-01 17:51:33 +0900

    RatingModel 작업하기

--- lib/common/utils/data_utils.dart
+++ lib/common/utils/data_utils.dart
@@ -1,5 +1,8 @@
 import 'package:actual/common/const/data.dart';
 
 class DataUtils {
-  static pathToUrl(String value) => 'http://$ip$value';
+  static String pathToUrl(String value) => 'http://$ip$value';
+
+  static List<String> listPathsToUrls(List<String> paths) =>
+      paths.map((e) => pathToUrl(e)).toList();
 }
---
+++ lib/user/model/user_model.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:actual/common/utils/data_utils.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
+class UserModel {
+  final String id;
+  final String username;
+  @JsonKey(fromJson: DataUtils.pathToUrl)
+  final String imageUrl;
+
+  UserModel({
+    required this.id,
+    required this.username,
+    required this.imageUrl,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) =>
+      _$UserModelFromJson(json);
+}
