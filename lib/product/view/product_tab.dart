9dfada057d2d92e6de14a0f1c6ace414900a37ca
boni <kgb8594@gmail.com>
2022-09-02 10:48:26 +0900

    Product Pagination 세팅하기

---
+++ lib/product/view/product_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProductTab extends ConsumerStatefulWidget {
+  const ProductTab({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ProductTabState();
+}
+
+class _ProductTabState extends ConsumerState<ProductTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
