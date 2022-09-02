fe64363f77bff14c4da9cb67dc79cd8dc1364244
boni <kgb8594@gmail.com>
2022-09-02 14:53:54 +0900

    GoRouter 세팅하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:actual/common/view/root_tab.dart';
 import 'package:actual/common/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(
@@ -13,14 +15,35 @@ void main() {
 class _App extends StatelessWidget {
   const _App({Key? key}) : super(key: key);
 
+  GoRouter get _router => GoRouter(
+        initialLocation: '/splash',
+        routes: [
+          GoRoute(
+            path: '/splash',
+            builder: (_, state) => const SplashScreen(),
+          ),
+          GoRoute(
+            path: '/',
+            builder: (_, state) => const RootTab(),
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      // Route 정보 전달
+      routeInformationProvider: _router.routeInformationProvider,
+      // URI String 을 상태 및 Go Router 에서 사용할 수 있는 형태로
+      // 변환 해주는 함수
+      routeInformationParser: _router.routeInformationParser,
+      // 위에서 변경된 값으로 실제 어떤 라우트를 보여줄지 정하는 함수
+      routerDelegate: _router.routerDelegate,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'NotoSans',
       ),
-      home: const SplashScreen(),
+      // home: const SplashScreen(),
     );
   }
 }
